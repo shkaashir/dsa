ee164395243c8d7db70eb41ca65351f94a68a77b
Prabin Pandey <prabin.pandey@ekbana.info>
2022-05-05 15:51:36 +0545

    refactored linked list

--- LinkedList/singly_linked_list.py
+++ LinkedList/singly_linked_list.py
@@ -7,12 +7,13 @@ class Node:
         self.next = None
 
 class LinkList:
-    '''Linked list implementation as linked listg '''
+    '''Linked list with head and tail initialization'''
     def __init__(self):
         self.head = None
         self.tail = None
     
     def insert_at_start(self,data):
+        '''insert data at beginning of linked list'''
         node = Node(data)
         if self.head is None:
             self.head = node
@@ -22,6 +23,7 @@ class LinkList:
             self.head = node
 
     def insert_at_end(self,data):
+        '''Insert data at last of linked list'''
         node = Node(data)
         if self.head is None:
             self.head = node
@@ -30,6 +32,7 @@ class LinkList:
             self.tail.next = node
             self.tail = node
     def insert_at_anywhere(self,data,position):
+        '''INsert the data ant given position'''
         newNode = Node(data)
         headNode = self.head
         if (position < 0):
@@ -38,11 +41,12 @@ class LinkList:
             newNode = Node(data)
             newNode.next = headNode
             self.head = newNode
+            self.tail=newNode
         else:
             # Keep looping until the position is zero
             while (position != 0):
                 position -= 1
-                if (position == 1):
+                if (position == 0):
                     # Making the new Node to point to
                     # the old Node at the same position
                     newNode.next = headNode.next
@@ -53,11 +57,12 @@ class LinkList:
                 headNode = headNode.next
                 if headNode == None:
                     break
-            if position != 1:
+            if position != 0:
                 print("position out of range")
-        return self.head
+        return headNode
 
     def show(self):
+        '''to display all linked list'''
         node_val = ''
         head = self.head
         while head:
@@ -65,17 +70,18 @@ class LinkList:
             head = head.next
         return node_val
     def peek(self):
+        '''to get top most node of linkedlist'''
         if self.head is None:
             return 'No data'
         return self.head.data
 
 ll = LinkList()
-ll.insert_at_start(10)
+# ll.insert_at_start(10)
 ll.insert_at_start(20)
 ll.insert_at_start(30)
-ll.insert_at_anywhere(21,3)
-ll.insert_at_start(200)
-ll.insert_at_start(300)
+# ll.insert_at_start(200)
+# ll.insert_at_start(300)
 ll.insert_at_end(40)
+ll.insert_at_anywhere(21,3)
 print(ll.show())
 ll.peek()
